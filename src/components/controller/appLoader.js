0343027c51e2c4849b033d7373be5f1771718fc0
Павел <pavel@MacBook-Air-Pavel.local>
2022-07-14 13:27:28 +0300

    fix: add apikey to appLoader.js

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'f5f7820e386d4ecfbe1624cccdb05f5d', // получите свой ключ https://newsapi.org/
         });
     }
 }
